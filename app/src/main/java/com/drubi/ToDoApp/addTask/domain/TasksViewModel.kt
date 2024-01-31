880ad5d8b57d3ece740090f8ec0152ba15873997
Diego Rubi <diego.rubi@salud.gob.sv>
2024-01-31 11:01:12 -0600

    ✨ Ya se agregan las tareas

--- app/src/main/java/com/drubi/ToDoApp/addTask/domain/TasksViewModel.kt
+++ app/src/main/java/com/drubi/ToDoApp/addTask/domain/TasksViewModel.kt
@@ -1,8 +1,44 @@
 package com.drubi.ToDoApp.addTask.domain
 
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.drubi.ToDoApp.addTask.ui.model.TaskModel
 import javax.inject.Inject
 
 class TasksViewModel
 @Inject constructor() : ViewModel() {
+
+    private val _showDialog = MutableLiveData<Boolean>()
+    val showDialog: LiveData<Boolean> = _showDialog
+
+    // no usar live data en recycleview
+    private val _tasks = mutableStateListOf<TaskModel>()
+    val tasks: List<TaskModel> = _tasks
+    fun onDialogClose() {
+        _showDialog.value = false
+    }
+
+    fun onTaskCreated(it: String) {
+        Log.i("TasksViewModel", "onTaskAdded: $it")
+        _tasks.add(TaskModel(
+            task = it
+        ))
+        onDialogClose()
+    }
+
+    fun onShowDialogClick() {
+        _showDialog.value = true
+    }
+
+    fun onCheckBoxSelected(taskModel: TaskModel) {
+        val index = _tasks.indexOf(taskModel)
+        _tasks[index] = _tasks[index].let {
+            it.copy(selected = !it.selected)
+        }
+    }
+
+
 }
--- app/src/main/java/com/drubi/ToDoApp/addTask/ui/TasksScreen.kt
+++ app/src/main/java/com/drubi/ToDoApp/addTask/ui/TasksScreen.kt
@@ -3,20 +3,27 @@ package com.drubi.ToDoApp.addTask.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -28,20 +35,70 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.drubi.ToDoApp.addTask.domain.TasksViewModel
+import com.drubi.ToDoApp.addTask.ui.model.TaskModel
 
 
 @Composable
 fun TasksScreen(tasksViewModel: TasksViewModel) {
-    AddTasksDialog(show = true, onDismiss = {}, onTaskAdded = {})
+    val showDialog by tasksViewModel.showDialog.observeAsState(false)
+
     Box(modifier = Modifier.fillMaxSize()) {
-        FabDialog(Modifier.align(Alignment.BottomEnd))
+        AddTasksDialog(
+            show = showDialog,
+            onDismiss = {
+                tasksViewModel.onDialogClose()
+            },
+            onTaskAdded = {
+                tasksViewModel.onTaskCreated(it)
+            })
+        FabDialog(Modifier.align(Alignment.BottomEnd), tasksViewModel)
+        TaskList(tasksViewModel)
     }
 }
 
 @Composable
-fun FabDialog(modifier: Modifier) {
+fun TaskList(tasksViewModel: TasksViewModel) {
+    val myTasks: List<TaskModel> = tasksViewModel.tasks
+    LazyColumn() {
+        items(myTasks, key = { it.id }) { task ->
+            ItemTask(taskModel = task, tasksViewModel = tasksViewModel)
+        }
+    }
+}
+
+@Composable
+fun ItemTask(taskModel: TaskModel, tasksViewModel: TasksViewModel) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        elevation = CardDefaults.cardElevation(
+            disabledElevation = 8.dp,
+            draggedElevation = 8.dp,
+            focusedElevation = 8.dp,
+            hoveredElevation = 8.dp,
+            pressedElevation = 8.dp,
+        )
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = taskModel.task, modifier = Modifier
+                    .weight(1f)
+
+            )
+            Checkbox(checked = taskModel.selected, onCheckedChange = {
+                tasksViewModel.onCheckBoxSelected(taskModel)
+            })
+        }
+    }
+}
+
+@Composable
+fun FabDialog(modifier: Modifier, tasksViewModel: TasksViewModel) {
     FloatingActionButton(
-        onClick = { /*TODO*/ },
+        onClick = {
+            tasksViewModel.onShowDialogClick()
+        },
         modifier = modifier
     ) {
         Icon(Icons.Filled.Add, contentDescription = "")
@@ -49,7 +106,7 @@ fun FabDialog(modifier: Modifier) {
 }
 
 @Composable
-fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded:(String)->Unit) {
+fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
     var myTasks by rememberSaveable {
         mutableStateOf("")
     }
@@ -68,16 +125,17 @@ fun AddTasksDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded:(String)->U
                     fontWeight = FontWeight.Bold
                 )
                 Spacer(modifier = Modifier.size(16.dp))
-                        TextField(
-                            value = myTasks,
-                            onValueChange = { myTasks = it },
-                            singleLine = true,
-                            maxLines = 1,
-                        )
+                TextField(
+                    value = myTasks,
+                    onValueChange = { myTasks = it },
+                    singleLine = true,
+                    maxLines = 1,
+                )
                 Spacer(modifier = Modifier.size(16.dp))
                 Button(
                     onClick = {
                         onTaskAdded(myTasks)
+                        myTasks = ""
                     },
                     modifier = Modifier.fillMaxWidth()
                 ) {
